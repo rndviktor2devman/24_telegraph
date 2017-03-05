45599028ed0123684e404dc8e93a96d41a63a7cf
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-05 11:22:43 +0300

    server.py

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def form():
     return render_template('form.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
