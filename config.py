4a846e61b5d74ea7f9460e9052f841815d21dc4c
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-06 22:49:07 +0300

    add autocreate script

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite://app.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
