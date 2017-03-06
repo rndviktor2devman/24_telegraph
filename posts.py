02820e48be53e7cdd73654edb199e112825a52da
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-03-06 22:59:03 +0300

    added passphrase

--- posts.py
+++ posts.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import string
+import random
 
 db = SQLAlchemy()
 
@@ -10,11 +12,19 @@ class Post(db.Model):
     title = db.Column(db.String(120))
     text = db.Column(db.Text)
     update_date = db.Column(db.DateTime)
+    passphrase = db.Column(db.Strin(80))
 
-    def __init__(self, author, title, text, date=None):
+    def __init__(self, author, title, text, date=None, passphrase=None):
         self.author = author
         self.title = title
         self.text = text
         if date is None:
             date = datetime.utcnow()
         self.update_date = date
+        if passphrase is None:
+            passphrase = id_generator()
+        self.passphrase = passphrase
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
